a51b5bd98e3f8ebb87407864030375fe8e495dd9
William Chabla <w_chabla@hotmail.com>
2018-03-13 20:30:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('.target')[0];
+}
+
+function increaseRankBy(n) {
+  var rankedItems = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for ( var i = 0; i < rankedItems.length; i++) {
+    rankedItems[i].innerHTML = parseInt(rankedItems[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var temp = document.getElementById('grand-node').querySelectorAll('div').innerHTML;
+  
+  console.log(temp);
+}
+
+//deepestChild();
+
+document.getElementById('grand-node');
+
