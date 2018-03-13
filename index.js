9d996d7f09f7689530c3da9385ea12ff9f551d75
William Chabla <w_chabla@hotmail.com>
2018-03-13 20:44:42 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('.target')[0];
+}
+
+function increaseRankBy(n) {
+  var rankedItems = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  
+  for ( var i = 0; i < rankedItems.length; i++) {
+    rankedItems[i].innerHTML = parseInt(rankedItems[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var temp = document.getElementById('grand-node');
+  
+  while (typeof(temp) === 'object') {
+    if (!temp.querySelector('div')) {
+      return temp;
+    }
+    temp = temp.querySelector('div');
+  }
+  
+  //return temp.innerHTML;
+}
+
